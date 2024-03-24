c803a4f5e8577d0b78df9da77b7ccfab0876961b
thanhthuy280501 <156226788+thanhthuy280501@users.noreply.github.com>
2024-03-23 22:21:09 -0400

    Update Lab15Functions.cpp

--- Lab15Functions.cpp
+++ Lab15Functions.cpp
@@ -1,28 +1,34 @@
-/*******************************************************************
-* getInfo *
-* Gets and validates lottery info from the user and places it in   *
-* reference parameters referencing variables in the main function. *
-*******************************************************************/
-void getInfo(int & pickFrom, int & numPicks){
+#include <iostream>
+//function propotypes
+void getInfor(int &pickFrom, int &numPicks);
+double computeWays(int n, int k);
+double factorial (int n);
 
+int main(){
+    int pickFrom, numPicks;
+    getInfo (pickFrom, numPicks); //Get information about the lottery
+    double ways= computeWays (pickFrom, numPicks); //Calculate the number of possible combinations
+    std::cout << "Number if possible combinations: " << ways << std::end;
+    return 0;
 }
 
-/******************************************************************
-* computeWays                                                     *
-* Computes and returns the number of different possible sets      *
-* of k numbers that can be chosen from a set of n numbers.        *
-* The formula for this is     k!(n- k)!                           *
-*                             --------                            *
-*                                 n!                              *
-*******************************************************************/
-// Note that the computation is done in a way that does not require
-// multiplying two factorials together. This is done to prevent any
-// intermediate result becoming so large that it causes overflow.
+//gets and validates lottery info from the user and places it in reference parameteres
+void getInfo(int &pickFrom, int &numPicks){
+    std::cout << "Enter the toal number of numbers to pick from: ";
+    std::cin >>pickfrom;
+    std::cout << "Enter the number of numbers to pick: ";
+    std::cin >>numPicks;
+}
+//computes and returns the number of different possible sets of k numbers that can be chosen from a set of n numbers.
 double computeWays(int n, int k){
-    
+    //using the formula: n!/(k!*(n-k)!)
+    double ways= factorial(n)/(factorial(k)*factorial(n-k));
+    return ways;
 }
-
-// This function computes factorials recursively. It is called by computeWays.
+//recursive function to compute factorials 
 double factorial(int n){
-
+    if(n==0)
+        return 1;
+    else
+        return n*factorial(n-1);
 }
